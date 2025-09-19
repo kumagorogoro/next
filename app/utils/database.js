8f203e6b9bc4162c7585c5b77435c32cd20e17d9
kumagorogoro <marustage.ooo@gmail.com>
2025-09-20 07:05:29 +0900

    backendfrontendcreate

--- app/utils/database.js
+++ app/utils/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://marustageooo:1912a1025B@cluster0.ituyud7.mongodb.net/nextAppDataBase?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://marustageooo:korocorocoron2010@cluster0.ituyud7.mongodb.net/nextAppDataBase?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Success:Connected to MongoDB");
   } catch {
