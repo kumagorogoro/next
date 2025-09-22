5fc21359ce27f04ffbed5604fe0489fb0b00d597
kumagorogoro <marustage.ooo@gmail.com>
2025-09-22 21:34:49 +0900

    headerwidth

--- app/components/header.js
+++ app/components/header.js
@@ -1,23 +1,35 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
-const Header = () =>{
-    return(
-        <header>
-            <div>
-                <Link href="/">
-                <Image src="/header.svg" eidth={1330} height={148} alt="header-image" priority/>
-                </Link>
-            </div>
-            <nav>
-                <ul>
-                    <li><Link href="/user/register">登録</Link></li>
-                    <li><link href="/user/login">ログイン</link></li>
-                    <li><link href="/item/create">アイテム作成</link></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <Link href="/">
+          <Image
+            src="/header.svg"
+            width={1330}
+            height={148}
+            alt="header-image"
+            priority
+          />
+        </Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/user/register">登録</Link>
+          </li>
+          <li>
+            <link href="/user/login">ログイン</link>
+          </li>
+          <li>
+            <link href="/item/create">アイテム作成</link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
