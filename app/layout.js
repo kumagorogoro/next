ced45613bfe4606966a1c083a024ad3522bf951b
kumagorogoro <marustage.ooo@gmail.com>
2025-09-22 21:29:47 +0900

    fontcomentout

--- app/layout.js
+++ app/layout.js
@@ -3,17 +3,17 @@ import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-import { Inter } from "next/font/google"; // ✅ Inter フォントをインポート
+//import { Inter } from "next/font/google"; // ✅ Inter フォントをインポート
 
-const Inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter", // 任意の変数名
-});
+//const Inter = Inter({
+//subsets: ["latin"],
+//variable: "--font-inter", // 任意の変数名
+//});
 
-export const metadata = {
-  title: "My App",
-  description: "Using Inter font from Google Fonts",
-};
+//export const metadata = {
+//title: "My App",
+//description: "Using Inter font from Google Fonts",
+//};
 
 const RootLayout = ({ children }) => {
   return (
